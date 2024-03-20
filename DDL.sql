14da2a54ad21fab7b11215519962e2203579d33b
MVillagomez56 <99492440+MVillagomez56@users.noreply.github.com>
2024-03-20 08:51:47 -0400

    update ddl

--- DDL.sql
+++ DDL.sql
@@ -2,7 +2,6 @@
 CREATE TYPE classType AS ENUM ('personal', 'group');
 CREATE TYPE status AS ENUM ('available', 'unavailable', 'maintenance');
 CREATE TYPE service AS ENUM ('membership', 'personal fitness class', 'group fitness class');
-
 CREATE TYPE role AS ENUM ('admin', 'member', 'trainer');
 
 
@@ -18,7 +17,6 @@ CREATE TABLE Users (
 
 CREATE TABLE Trainers (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
     specialization TEXT NOT NULL,
     monday_availability DATERANGE,
     tuesday_availability DATERANGE,
@@ -28,17 +26,16 @@ CREATE TABLE Trainers (
     saturday_availability DATERANGE,
     sunday_availability DATERANGE,
     cost INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES Users(id)
+    FOREIGN KEY(id) REFERENCES Users(id)
 );
 
 CREATE TABLE Members (
     id SERIAL PRIMARY KEY ,
-    user_id INTEGER NOT NULL,
-    weight INTEGER NOT NULL,
-    height INTEGER NOT NULL,
-    muscle_mass INTEGER NOT NULL,
-    body_fat INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES Users(id)
+    weight INTEGER,
+    height INTEGER,
+    muscle_mass INTEGER,
+    body_fat INTEGER,
+    FOREIGN KEY(id) REFERENCES Users(id)
 );
 
 CREATE TABLE Rooms (
@@ -84,6 +81,7 @@ CREATE TABLE Payments (
     amount INTEGER NOT NULL,
     date DATE NOT NULL,
     service TEXT NOT NULL,
+    completion_status BOOLEAN NOT NULL DEFAULT FALSE, -- 0 for pending, 1 for completed
     FOREIGN KEY(member_id) REFERENCES Members(id)
 );
 

244da05c59f73c1a6163d497647c5f255717eb57
MVillagomez56 <99492440+MVillagomez56@users.noreply.github.com>
2024-03-21 23:26:14 -0400

    Add Banner component to Dashboard

---
+++ frontend/3005/src/components/Banner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import hero from '../assets/hero_image.png';
+
+const Banner = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffc99d',
+        color: 'white',
+        padding: '20px',
+        gap: '20px',
+      }}
+    >
+        <img src={hero} alt="hero" />
+      <Typography variant="h1" width={'50%'} color={'black'}>
+        Muha Muha Gym
+      </Typography>
+     
+    </Box>
+  );
+};
+
+export default Banner;
--- frontend/3005/src/pages/Dashboard.tsx
+++ frontend/3005/src/pages/Dashboard.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Banner from '../components/Banner'
 
 
 export const Dashboard = () => {
     return (
         <div>
-        <h1>Dashboard</h1>
+        <Box>
+          <Banner />
+        </Box>
         </div>
     )
  }
